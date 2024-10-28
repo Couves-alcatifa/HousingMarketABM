895deabea483fbe9e7e5f3f21cd142ed9900fa7a
Couves-alcatifa <miguel.alves01@hotmail.com>
2024-10-28 21:52:33 +0000

    prepare calibration

--- runCalibration.sh
+++ runCalibration.sh
@@ -1,30 +1,14 @@
-python generateExogenousVariables.py 400 0.4 0.25 1.15 0.1 1.5 0.7 0.7 0.01 50 200
+python generateExogenousVariables.py 400 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 30 10
 time julia --threads 8 economy.jl
-python generateExogenousVariables.py 400 0.4 0.25 1.2 0.1 1.5 0.7 0.7 0.01 50 200
+python generateExogenousVariables.py 600 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 30 10
 time julia --threads 8 economy.jl
-python generateExogenousVariables.py 400 0.4 0.25 1.2 0.05 1.5 0.7 0.7 0.01 50 200
+python generateExogenousVariables.py 700 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 30 10
 time julia --threads 8 economy.jl
-python generateExogenousVariables.py 400 0.4 0.25 1.15 0.1 1.4 0.7 0.7 0.01 50 200
+python generateExogenousVariables.py 800 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 30 10
 time julia --threads 8 economy.jl
-python generateExogenousVariables.py 400 0.4 0.25 1.15 0.1 1.8 0.7 0.7 0.01 50 200
+python generateExogenousVariables.py 900 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 30 10
 time julia --threads 8 economy.jl
-python generateExogenousVariables.py 400 0.4 0.25 1.15 0.1 2.0 0.7 0.7 0.01 50 200
+python generateExogenousVariables.py 600 0.5 0.5 1.1 0.1 1.0 0.6 0.6 0.01 30 10
 time julia --threads 8 economy.jl
-python generateExogenousVariables.py 400 0.4 0.25 1.15 0.1 2.5 0.7 0.7 0.01 50 200
-time julia --threads 8 economy.jl
-python generateExogenousVariables.py 400 0.4 0.25 1.15 0.1 1.5 0.6 0.7 0.01 50 200
-time julia --threads 8 economy.jl
-python generateExogenousVariables.py 400 0.4 0.25 1.15 0.1 1.5 0.8 0.7 0.01 50 200
-time julia --threads 8 economy.jl
-python generateExogenousVariables.py 400 0.4 0.25 1.15 0.1 1.5 0.7 0.7 0.005 50 200
-time julia --threads 8 economy.jl
-python generateExogenousVariables.py 400 0.4 0.25 1.15 0.1 1.5 0.7 0.7 0.01 50 400
-time julia --threads 8 economy.jl
-python generateExogenousVariables.py 400 0.4 0.25 1.15 0.1 1.5 0.7 0.7 0.01 50 800
-time julia --threads 8 economy.jl
-python generateExogenousVariables.py 400 0.4 0.25 1.15 0.1 1.5 0.7 0.7 0.01 200 400
-time julia --threads 8 economy.jl
-python generateExogenousVariables.py 400 0.4 0.25 1.15 0.1 1.5 0.7 0.7 0.01 600 200
-time julia --threads 8 economy.jl
-python generateExogenousVariables.py 400 0.4 0.25 1.15 0.1 1.5 0.7 0.7 0.01 800 200
+python generateExogenousVariables.py 400 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 10 5
 time julia --threads 8 economy.jl
