5c874d77792e0f83998ce2baa512de4a8200ac63
Couves-alcatifa <miguel.alves01@hotmail.com>
2024-11-24 11:42:35 +0000

    calibration run

--- runCalibration.sh
+++ runCalibration.sh
@@ -1,14 +1,10 @@
-python generateExogenousVariables.py 400 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 30 10
+python generateExogenousVariables.py 400 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 30 10 12.0 6.0
 time julia --threads 8 economy.jl
-python generateExogenousVariables.py 600 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 30 10
+python generateExogenousVariables.py 400 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 70 40 12.0 6.0
 time julia --threads 8 economy.jl
-python generateExogenousVariables.py 700 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 30 10
+python generateExogenousVariables.py 400 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 80 40 12.0 6.0
 time julia --threads 8 economy.jl
-python generateExogenousVariables.py 800 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 30 10
+python generateExogenousVariables.py 400 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 80 60 12.0 6.0
 time julia --threads 8 economy.jl
-python generateExogenousVariables.py 900 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 30 10
-time julia --threads 8 economy.jl
-python generateExogenousVariables.py 600 0.5 0.5 1.1 0.1 1.0 0.6 0.6 0.01 30 10
-time julia --threads 8 economy.jl
-python generateExogenousVariables.py 400 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 10 5
+python generateExogenousVariables.py 400 0.4 0.25 1.1 0.1 1.0 0.6 0.6 0.01 60 30 18.0 9.0
 time julia --threads 8 economy.jl
