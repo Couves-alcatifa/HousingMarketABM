6ae3a1aff640676cfc18e7d78ce0fb03e272fbc2
Couves-alcatifa <miguel.alves01@hotmail.com>
2025-01-09 19:40:43 +0000

    use mean instead of average when calculating market price

--- utilities.jl
+++ utilities.jl
@@ -27,7 +27,7 @@ function calculate_market_price(model, house)
         return calculate_initial_market_price(house) * INITIAL_MARKET_PRICE_CUT[house.location]
     end
     # println("house = $house mean(transactions) * house.area * house.maintenanceLevel = $(mean(transactions) * house.area * house.maintenanceLevel)")
-    return median(bucket) * house.area * 
+    return mean(bucket) * house.area * 
            map_value((house.percentile - 1) % 25, 0, 24, 0.90, 1.10)
 end
 
