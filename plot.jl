21b808e845474460b0283048ff3b1c6d3460cb77
Couves-alcatifa <miguel.alves01@hotmail.com>
2025-01-16 08:36:33 +0000

    plot real values

--- plot.jl
+++ plot.jl
@@ -9,9 +9,13 @@ function scatter_plot(x, y)
 end
 
 quarters = [quarter for quarter in 1:12]
+years = [year for year in 1:10]
 
 prices_in_oeiras = [2440, 2467, 2550, 2644, 2721, 2822, 2929, 3001, 3093, 3145, 3177, 3158]
 simulated_prices_in_oeiras = [2278,2310,2319,2363,2430,2561,2696,2872,3011,3117,3313,3399]
+pricesFrom2003 = [81532, 96634, 111347, 121298, 124405, 125992, 115405, 118345, 100709, 95297]
 
-save("RealPricesInOeiras.png", scatter_plot(quarters, prices_in_oeiras))
-save("SimulatedPricesInOeiras.png", scatter_plot(quarters, simulated_prices_in_oeiras))
+save("PricesFrom2003.png", scatter_plot(years, pricesFrom2003))
+# save("RealPricesInOeiras.png", scatter_plot(quarters, prices_in_oeiras))
+# save("SimulatedPricesInOeiras.png", scatter_plot(quarters, simulated_prices_in_oeiras))
+# save("SimulatedPricesInOeiras.png", scatter_plot(quarters, simulated_prices_in_oeiras))
