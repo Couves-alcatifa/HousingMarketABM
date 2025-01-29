7aed2cd167707a07f14ea9c5792e9d15f1f6d4cb
Couves-alcatifa <miguel.alves01@hotmail.com>
2025-01-29 19:28:56 +0000

    run in Amadora

--- runAllLocations.sh
+++ runAllLocations.sh
@@ -1,5 +1,6 @@
 # locations=("Amadora" "Cascais" "Lisboa" "Loures" "Mafra" "Odivelas" "Oeiras" "Sintra" "VilaFrancaDeXira" "Alcochete" "Almada" "Barreiro" "Moita" "Montijo" "Palmela" "Seixal" "Sesimbra" "Setubal")
-locations=("Palmela" "Seixal", "Sesimbra")
+locations=("Amadora" "Cascais" "Loures" "Mafra" "VilaFrancaDeXira" "Alcochete" "Barreiro" "Moita" "Palmela" "Seixal" "Sesimbra" "Setubal")
+# locations=("Palmela" "Seixal", "Sesimbra")
 for location in ${locations[@]}
 do
     python generateScope.py $location
