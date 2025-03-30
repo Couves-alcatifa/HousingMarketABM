97a18af3813674485eb8349eae75204e954bd20b
miguel <miguel.alves01@hotmail.com>
2025-03-30 21:31:40 +0100

    run in Oeiras with Baseline

--- runPolicies.sh
+++ runPolicies.sh
@@ -3,7 +3,7 @@ locations=("Oeiras")
 
 # policies=("Baseline" "ConstructionVatReduction" "ConstructionLicensingSimplification" "RentSubsidy" "NonResidentsProhibition")
 # policies=("ConstructionVatReduction" "ConstructionLicensingSimplification" "RentSubsidy" "NonResidentsProhibition")
-policies=("ConstructionLicensingSimplification")
+policies=("Baseline" "ConstructionLicensingSimplification")
 for location in ${locations[@]}
 do
     for policy in ${policies[@]}
